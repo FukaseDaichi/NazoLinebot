df2c0dc0d56926a24aa3f096b2145a198809cb4d
ITSVAPP <it.da.fukase@iftc.co.jp>
2024-12-18 09:42:03 +0900

    Refactor user registration process in messages_register_user.py by introducing partial function application for improved clarity and removing redundant arguments in threading. This change enhances the readability and maintainability of the code.

--- src/messages/messages_register_user.py
+++ src/messages/messages_register_user.py
@@ -1,3 +1,4 @@
+from functools import partial
 from urllib.parse import unquote
 from flask import g
 from src.messages.messages_normal import Message as NormalMessage
@@ -18,10 +19,10 @@ class Message:
         g.user_state_manager.set_user_state(
             g.user_id, {"user_name": user_input, "mode": "default"}
         )
-        
-        target = g.gas_manager.register_user
+        # 値を束縛した新しい関数を作成
+        target = partial(g.gas_manager.register_user, g.user_id, user_input)
         # スレッドを作成して非同期で実行
-        thread = threading.Thread(target=target, args=(g.user_id, user_input))
+        thread = threading.Thread(target=target)
         thread.start()
 
         return NormalMessage.create_message(event, f"{user_input}で登録しました。")
